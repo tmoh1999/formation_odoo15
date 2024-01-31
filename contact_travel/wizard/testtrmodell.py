f2f99e98299492d27fe21ac4a228d30de4f49708
TOUABTImohamed <mohamed.touabti@weasydoo.com>
2024-01-31 14:14:17 +0100

    video id139TOid146/tested orm methods:create/browse/write/unlink/search/search_count/fields_get

--- contact_travel/wizard/testtrmodell.py
+++ contact_travel/wizard/testtrmodell.py
@@ -11,4 +11,33 @@ class TestTransModell(models.TransientModel):
     def action_test_trans(self):
         print("Button clicked:Lunching Action")
         action = self.env.ref("contact_travel.action_voyage").read()[0]
+        #orm create
+        self.env["voyage"].create({'name':'TestORMCREATE','montant':456.8,'voyageur_id':3})
+        #orm browse
+        res=self.env["voyage"].browse([10,28])
+        for rec in res:
+            print(rec.name,rec.montant)
+        #orm write
+        res = self.env["voyage"].browse([19])
+        if res.exists():
+            res.write({'name':'mohamed'})
+        #orm unlink
+        res = self.env["voyage"].browse([19])
+        if res.exists():
+            res.unlink()
+        #orm search
+        res=self.env["voyage"].search([('name','=',"mohamed")])
+        for rec in res:
+            print(rec.id,rec.name,rec.montant)
+        #orm search_count
+        res = self.env["voyage"].search_count([('name', '=', "mohamed")])
+        print(res)
+        #orm fields_get
+        res=self.env["voyage"].fields_get([('name','montant'),('type','string')])
+        print(res)
+        #orm metadata
+        res=self.env["voyage"].browse([28])
+        print(res.get_metadata())
+
+
         return action
